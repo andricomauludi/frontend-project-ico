927a08cb99c762e47ddcdd333164eb63cd90f588
andricomauludi <mauludi21@gmail.com>
2023-06-08 13:46:20 +0700

    improve table

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,25 +1,55 @@
-import React from 'react'
-import BootstrapTable from 'react-bootstrap-table-next';
-import { Container } from 'reactstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import { Container, Button } from "reactstrap";
+import { faInfo } from '@fortawesome/free-solid-svg-icons'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const columns = [{
-  dataField: 'id',
-  text: 'ID'
-}, {
-  dataField: 'nama',
-  text: 'Nama'
-}, {
-  dataField: 'alamat',
-  text: 'Alamat'
-}];
+const columns = [
+  {
+    dataField: "id",
+    text: "ID",
+    headerStyle:()=>{
+        return { width:"5%"};
+    },
+},{
+    dataField: "nama",
+    text: "Nama",
+  },
+  {
+    dataField: "alamat",
+    text: "Alamat",
+  },
+  {
+    dataField: "link",
+    text: "Action",
+    formatter: (rowContent, row) => {
+      return (
+        <div>
+          <Button color="dark" className="mr-2">
+            <FontAwesomeIcon icon={faInfo} /> Detail
+          </Button>
 
+          <Button color="dark" className="mr-2">
+            <FontAwesomeIcon icon={faEdit} /> edit
+          </Button>
+
+          <Button color="dark" className="mr-2">
+            <FontAwesomeIcon icon={faTrash} /> Delete
+          </Button>
+        </div>
+      );
+    },
+  },
+];
 
 const TableComponent = (props) => {
   return (
     <Container>
-        <BootstrapTable keyField='id' data={ props.users } columns={ columns } />
+      <BootstrapTable keyField="id" data={props.users} columns={columns} />
     </Container>
-  )
-}
+  );
+};
 
-export default TableComponent
+export default TableComponent;
