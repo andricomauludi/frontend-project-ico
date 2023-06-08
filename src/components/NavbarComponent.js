6f75829d4b005a71e4caaa621da961a1bd646426
andricomauludi <mauludi21@gmail.com>
2023-06-08 10:41:12 +0700

    reactstrap start

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+
+function NavbarComponent(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar {...args}>
+        <NavbarBrand href="/">Mornin</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                About Us
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Admin</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavbarComponent;
